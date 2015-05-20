946e968a8487b42d43a9d49e2a493a568ca3ba45
hainguyen1981 <nguyenbuihai@yahoo.com>
2015-05-20 07:16:52 +0700

    Mapping hibernate

--- trunk/source/dcap/ws-server/view/Service1.svc.cs
+++ trunk/source/dcap/ws-server/view/Service1.svc.cs
@@ -6,7 +6,7 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 
-namespace svc_dcap
+namespace ws_server.view
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     public class Service1 : IService1
