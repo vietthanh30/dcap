6fe9ffb7677c0728b5db26840745e1dd1fc4b60c
vietthanh30 <vietthanh30@gmail.com>
2015-05-28 01:58:38 +0700

    change mapping file

---
+++ trunk/source/dcap/ws-server/model/ManagerL1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ws_server.model
+{
+    public class ManagerL1 : ManagerLevel
+    {
+
+    	#region Constructor
+
+        public ManagerL1()
+        {
+        }
+
+    	#endregion
+
+    }
+}
