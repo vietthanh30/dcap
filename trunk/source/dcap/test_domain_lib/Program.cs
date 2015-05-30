0dc6557a7043f5830c3ceae5252e5a12ec4780d2
hainguyen1981 <nguyenbuihai@yahoo.com>
2015-05-30 11:44:29 +0700

    Add test_domain_lib project

---
+++ trunk/source/dcap/test_domain_lib/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using domain_lib.controller;
+
+namespace test_domain_lib
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new Controller();
+            String result = service.login("nguyenbh", "nguyenbh");
+            Console.Out.WriteLine("login code: " + result);
+            result = service.login("nguyenbh", "123456");
+            Console.Out.WriteLine("login code: " + result);
+            result = service.changePassword("nguyenbh", "nguyenbh", "123456", "123456");
+            Console.Out.WriteLine("changePassword code: " + result);
+            result = service.changePassword("nguyenbh", "123456", "123456", "123456");
+            Console.Out.WriteLine("changePassword code: " + result);
+            result = service.changePassword("nguyenbh", "123456", "nguyenbh", "nguyenbh");
+            Console.Out.WriteLine("changePassword code: " + result);
+            result = service.changePassword("nguyenbh", "nguyenbh", "123456", "123456");
+            Console.Out.WriteLine("changePassword code: " + result);
+        }
+    }
+}
