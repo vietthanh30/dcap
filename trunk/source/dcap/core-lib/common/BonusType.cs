44c49cf5058e4203c1fb8cd466109975245ed074
hainguyen1981 <nguyenbuihai@yahoo.com>
2015-06-02 05:58:05 +0700

    Add BonusType.cs

---
+++ trunk/source/dcap/core-lib/common/BonusType.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core_lib.common
+{
+    public class BonusType
+    {
+        #region Declarations
+
+        // Member variables
+
+        private string _type = string.Empty;
+
+        private double _amount = -1;
+
+        #endregion
+
+    	#region Constructor
+
+        public BonusType()
+        {
+        }
+
+    	#endregion
+
+        #region Properties
+        
+        /// <summary>
+        /// Type
+        /// </summary>
+        public virtual string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        /// <summary>
+        /// Amount
+        /// </summary>
+        public virtual double Amount
+        {
+            get { return _amount; }
+            set { _amount = value; }
+        }
+
+        #endregion
+
+        #region Method Overrides
+
+        public override string ToString()
+        {
+            return _type;
+        }
+
+        #endregion
+    }
+}
