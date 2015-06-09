6a0d301a9a6016fb4f3c6956f290531e37b2b6bc
hainguyen1981 <nguyenbuihai@yahoo.com>
2015-06-09 09:10:11 +0700

    Add UserUtil class

---
+++ trunk/source/dcap/web-app/common/UserUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using core_lib.common;
+using web_app.DcapServiceReference;
+
+namespace web_app.common
+{
+    public class UserUtil
+    {
+        public static string GetRoleCode(UserDto userDto)
+        {
+            if (userDto == null)
+            {
+                return ConstUtil.QLTV;
+            }
+            var allRoles = userDto.AllRoles;
+            if (allRoles.Length == 0)
+            {
+                return ConstUtil.QLTV;
+            }
+            return allRoles[0].RoleCode;
+        }
+
+        public static bool IsQthtRole(UserDto userDto)
+        {
+            return string.Compare(GetRoleCode(userDto), ConstUtil.QTHT, true) == 0;
+        }
+
+        public static bool IsQlktRole(UserDto userDto)
+        {
+            return string.Compare(GetRoleCode(userDto), ConstUtil.QLKT, true) == 0;
+        }
+
+        public static bool IsQltvRole(UserDto userDto)
+        {
+            return string.Compare(GetRoleCode(userDto), ConstUtil.QLTV, true) == 0;
+        }
+    }
+}
